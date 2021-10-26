edd64f4813aa4257b5c27af2e761b665eb009382
enikebraimoh <richardbraimoh@gmail.com>
2021-10-26 16:42:10 +0100

    implemented design

---
+++ app/src/main/java/com/adashi/escrow/ui/auth/PhoneAuth1Fragment.kt
@@ -0,0 +1,10 @@
+package com.adashi.escrow.ui.auth
+
+import com.adashi.escrow.R
+import com.adashi.escrow.databinding.FragmentPhoneAuth1Binding
+import ng.adashi.core.BaseFragment
+
+class PhoneAuth1Fragment : BaseFragment<FragmentPhoneAuth1Binding>(R.layout.fragment_phone_auth1) {
+
+
+}
---
+++ app/src/main/java/com/adashi/escrow/ui/auth/PhoneAuth2Fragment.kt
@@ -0,0 +1,9 @@
+package com.adashi.escrow.ui.auth
+
+import com.adashi.escrow.R
+import com.adashi.escrow.databinding.FragmentPhoneAuth2Binding
+import ng.adashi.core.BaseFragment
+
+class PhoneAuth2Fragment : BaseFragment<FragmentPhoneAuth2Binding>(R.layout.fragment_phone_auth2) {
+
+}
