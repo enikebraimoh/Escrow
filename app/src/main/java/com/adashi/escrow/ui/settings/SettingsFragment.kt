6092f9b21ba1652cb6b2c465e77498d2c2986635
enikebraimoh <richardbraimoh@gmail.com>
2021-10-31 02:17:51 +0100

    added banks screen

--- app/src/main/java/com/adashi/escrow/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/adashi/escrow/ui/settings/SettingsFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.adashi.escrow.R
 import com.adashi.escrow.databinding.FragmentSettingsBinding
 import ng.adashi.core.BaseFragment
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
+
+    override fun start() {
+        super.start()
+        binding.withdrawalbanks.setOnClickListener {
+            findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToWithdrawalBanksFragment())
+        }
+
+    }
 
 }
