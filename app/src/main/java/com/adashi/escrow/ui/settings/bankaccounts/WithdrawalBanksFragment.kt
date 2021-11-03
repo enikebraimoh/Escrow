e1e20f56c88b47394caf2ce3ec0381b7b11ec427
enikebraimoh <richardbraimoh@gmail.com>
2021-11-03 16:05:25 +0100

    added withdrawal fragment_withdrawal_banks.xml

--- app/src/main/java/com/adashi/escrow/ui/settings/bankaccounts/WithdrawalBanksFragment.kt
+++ app/src/main/java/com/adashi/escrow/ui/settings/bankaccounts/WithdrawalBanksFragment.kt
@@ -5,12 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.adashi.escrow.R
+import com.adashi.escrow.ShowSucessDialogFragment
 import com.adashi.escrow.databinding.FragmentWithdrawalBanksBinding
+import com.adashi.escrow.ui.addbank.AddBankDialogFragment
 import ng.adashi.core.BaseFragment
 
 class WithdrawalBanksFragment : BaseFragment<FragmentWithdrawalBanksBinding>(R.layout.fragment_withdrawal_banks){
 
+    override fun start() {
+        super.start()
 
+        binding.addBank.setOnClickListener {
+            var fr = AddBankDialogFragment{
+                when(it){
+                    0 ->{
+
+                    }
+                }
+            }
+            fr.show(requireActivity().supportFragmentManager,"added fragm")
+
+        }
+
+    }
 
 }
