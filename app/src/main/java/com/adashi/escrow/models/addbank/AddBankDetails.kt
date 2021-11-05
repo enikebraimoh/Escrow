29e26e3c1a7826672bb014bd2b193b95d4947738
enikebraimoh <richardbraimoh@gmail.com>
2021-11-05 13:38:47 +0100

    add banks

---
+++ app/src/main/java/com/adashi/escrow/models/addbank/AddBankDetails.kt
@@ -0,0 +1,7 @@
+package com.adashi.escrow.models.addbank
+
+data class AddBankDetails(
+    val accountName: String,
+    val accountNumber: String,
+    val bankName: String
+)
--- app/src/main/java/com/adashi/escrow/network/retrofit/AdashiApis.kt
+++ app/src/main/java/com/adashi/escrow/network/retrofit/AdashiApis.kt
@@ -1,14 +1,20 @@
 package ng.adashi.network.retrofit
 
 
+import com.adashi.escrow.models.accountname.AccountNameResponse
+import com.adashi.escrow.models.addbank.AddBankDetails
+import com.adashi.escrow.models.addbank.AddBankResponse
 import com.adashi.escrow.models.shipmentpatch.PatchShipingStatus
 import com.adashi.escrow.models.createtransaction.NewTransactionBodyResponse
 import com.adashi.escrow.models.createtransaction.Transaction
+import com.adashi.escrow.models.listofbanks.ListOfBanksItem
 import com.adashi.escrow.models.sampleTrans
 import com.adashi.escrow.models.shipmentpatch.ShipmentPatchResponse
 import com.adashi.escrow.models.signup.SignUpDetails
 import com.adashi.escrow.models.signup.SignUpResponse
 import com.adashi.escrow.models.user.UserResponse
+import com.adashi.escrow.models.verifybvn.BVN
+import com.adashi.escrow.models.verifybvn.BvnResponse
 import com.adashi.escrow.models.wallet.TransactionsResponse
 import com.adashi.escrow.models.wallet.WalletBalance
 import ng.adashi.domain_models.login.LoginDetails
@@ -41,10 +47,33 @@ interface AdashiApis {
     @GET("api/v1/auth/me")
     suspend fun getCurrentUser() : UserResponse
 
+    @GET("/")
+    suspend fun getNigerianBanks() : MutableList<ListOfBanksItem>
+
     @PATCH("api/v1/transactions/{transactionId}")
     suspend fun patchTransaction(
         @Path("transactionId") transactionId : String,
         @Body status : PatchShipingStatus
     ) : ShipmentPatchResponse
+
+    @GET("api/v1/others/validate-bvn/{bvn}")
+    suspend fun VerifyBvn(
+        @Path("bvn") bvn : String
+    ) : BvnResponse
+
+    @GET("api/v1/others/resolve-bank/{accountNumber}")
+    suspend fun checkAccountName(
+        @Path("accountNumber") number : String
+    ) : AccountNameResponse
+
+    @POST("api/v1/user/bvn")
+    suspend fun addBvn(
+        @Body bvn : BVN
+    ) : UserResponse
+
+    @POST("api/v1/user/add-bank")
+    suspend fun addBank(
+        @Body bank : AddBankDetails
+    ) : AddBankResponse
 
 }
