4ea985f7525116900ad847fbe407d8754cc98c4e
enikebraimoh <richardbraimoh@gmail.com>
2021-11-06 04:31:47 +0100

    add account fragment_withdrawal_banks.xml

---
+++ app/src/main/java/com/adashi/escrow/models/accountname/AccountNameResponse.kt
@@ -0,0 +1,7 @@
+package com.adashi.escrow.models.accountname
+
+data class AccountNameResponse(
+    val `data`: Data,
+    val message: String,
+    val status: String
+)
---
+++ app/src/main/java/com/adashi/escrow/models/addbank/Account.kt
@@ -0,0 +1,11 @@
+package com.adashi.escrow.models.addbank
+
+data class Account(
+    val __v: Int,
+    val _id: String,
+    val accountName: String,
+    val accountNumber: String,
+    val bankName: String,
+    val createdAt: String,
+    val updatedAt: String
+)
