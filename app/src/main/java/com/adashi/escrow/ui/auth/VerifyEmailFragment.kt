b78bd08cf3180db103e39d02d256c9df7db6101d
enikebraimoh <richardbraimoh@gmail.com>
2022-02-06 00:58:18 +0100

    added a screen to verify email after signup

---
+++ app/src/main/java/com/adashi/escrow/ui/auth/VerifyEmailFragment.kt
@@ -0,0 +1,17 @@
+package com.adashi.escrow.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.adashi.escrow.R
+import com.adashi.escrow.databinding.FragmentVerifyEmailBinding
+import ng.adashi.core.BaseFragment
+
+
+class VerifyEmailFragment : BaseFragment<FragmentVerifyEmailBinding>(R.layout.fragment_verify_email) {
+
+
+
+}
