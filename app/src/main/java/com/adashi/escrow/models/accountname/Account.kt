a64d27c1f5ae37514458b6165f83d4d08e67efaf
enikebraimoh <richardbraimoh@gmail.com>
2022-02-08 21:25:38 +0100

    added a lot of networking Endpoints, i forgot to commit each one 😶

---
+++ app/src/main/java/com/adashi/escrow/models/accountname/Account.kt
@@ -0,0 +1,6 @@
+package com.adashi.escrow.models.accountname
+
+data class Account(
+    val account_name: String,
+    val account_number: String
+)
--- app/src/main/java/com/adashi/escrow/models/addbank/Account.kt
+++ app/src/main/java/com/adashi/escrow/models/addbank/Account.kt
@@ -3,9 +3,10 @@ package com.adashi.escrow.models.addbank
 data class Account(
     val __v: Int,
     val _id: String,
-    val accountName: String,
-    val accountNumber: String,
-    val bankName: String,
+    val account_name: String,
+    val account_number: String,
+    val bank_code : String,
+    val bank_name: String,
     val createdAt: String,
     val updatedAt: String
 )
