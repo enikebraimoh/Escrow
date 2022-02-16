7b835a34863ad21809db9541b4b9dd63d31d6eea
enikebraimoh <richardbraimoh@gmail.com>
2022-02-16 22:55:48 +0100

    commit

--- app/src/main/java/com/adashi/escrow/utils/Constants.kt
+++ app/src/main/java/com/adashi/escrow/utils/Constants.kt
@@ -1,6 +1,6 @@
 package ng.adashi.utils
 
 object Constants {
-        const val SPLASH_SCREEN_TIMEER : Long = 3000
-        const val BASE_URL : String ="https://api.escroww.africa/API/v1/"
+    const val SPLASH_SCREEN_TIMEER: Long = 3000
+    const val BASE_URL = "https://api.escroww.africa/API/v1/"
 }
